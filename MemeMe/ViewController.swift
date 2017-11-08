e71ef6ad44c3feb095e8fcdef0578141aa290d5c
Angel Newton <xanewton@gmail.com>
2017-11-08 14:43:38 -0500

    Add image picker behaviour.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // MARK: Outlets
+    @IBOutlet weak var ImagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    // MARK: Memeber variables
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // disable camera button if it's not available
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.allowsEditing = false
+        pickerController.sourceType = .photoLibrary
+        pickerController.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(pickerController, animated: true, completion: nil)
     }
-
+    
+    
+    // MARK: ImagePicker cancel dialog
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil) // dismiss dialog
+    }
+    
+    // MARK: The user selected an image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage // get image
+        
+        ImagePickerView.contentMode = .center // scale type
+        ImagePickerView.image = chosenImage // set image to view
+        dismiss(animated:true, completion: nil) // dismiss dialog
+    }
+    
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+        imagePicker.cameraCaptureMode = .photo
+        imagePicker.modalPresentationStyle = .fullScreen
+        present(imagePicker, animated: true, completion: nil)
+    }
 
 }
 
